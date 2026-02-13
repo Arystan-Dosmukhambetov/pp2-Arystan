cf1dc1bb422dbb39f12b7e5b8a3650baab201a7a
Arystan <arystan544@gmail.com>
2026-02-13 21:52:50 +0500

    Update multiple_inheritance.py

--- Practice 3/inheritance/multiple_inheritance.py
+++ Practice 3/inheritance/multiple_inheritance.py
@@ -1,1 +1,14 @@
+class Father:
+    def skill(self):
+        print("Gardening")
 
+class Mother:
+    def talent(self):
+        print("Cooking")
+
+class Child(Father, Mother):
+    pass
+
+c = Child()
+c.skill()
+c.talent()

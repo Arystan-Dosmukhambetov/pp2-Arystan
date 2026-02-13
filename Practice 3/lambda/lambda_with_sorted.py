262c0be8087b180f592948fd97e6d6450550800f
Arystan <arystan544@gmail.com>
2026-02-13 21:49:07 +0500

    Update lambda_with_sorted.py

--- Practice 3/lambda/lambda_with_sorted.py
+++ Practice 3/lambda/lambda_with_sorted.py
@@ -1,1 +1,9 @@
+students = [
+    {"name": "Ali", "grade": 85},
+    {"name": "Sara", "grade": 92},
+    {"name": "Tom", "grade": 78}
+]
 
+sorted_students = sorted(students, key=lambda x: x["grade"])
+
+print(sorted_students)

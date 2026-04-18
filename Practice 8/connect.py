638359e1b57820cb3f4cbfd71236541ec9a017ba
Arystan <arystan544@gmail.com>
2026-04-18 10:59:21 +0500

    Create connect.py

---
+++ Practice 8/connect.py
@@ -0,0 +1,17 @@
+import psycopg2
+from config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+
+
+def connect():
+    try:
+        conn = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        return conn
+    except Exception as e:
+        print("Error connecting to database:", e)
+        return None
